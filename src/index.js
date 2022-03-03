607328409dd35b42d3a75e927a9a3b117955d69b
Jason Ng <jason.ng7952@gmail.com>
2022-03-03 18:54:11 -0500

    Added containers for each task and project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import UI from './modules/UI';
+import ToDoList from './modules/ToDoList';
+import Project from './modules/Project';
+import Task from './modules/Task';
+
+document.body.appendChild(UI.header());
+document.body.appendChild(UI.content());
+document.querySelector('#content').appendChild(UI.sidebar());
+document.querySelector('#content').appendChild(UI.currentProject());
+document.body.appendChild(UI.footer());
