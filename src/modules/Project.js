0b1974931c35066996b68f36df523c19164a2022
Jason Ng <jason.ng7952@gmail.com>
2022-03-04 18:58:23 -0500

    Add the basic functionality to add task to selected project

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,13 +1,31 @@
 class Project {
     
     tasks = [];
+    id = 0;
 
-    constructor(name) {
+    constructor(name, id) {
         this.name = name;
+        this.id = id;
     }
 
     addTask(task) {
         this.tasks.push(task);
+
+        const newTask = document.createElement('div'); 
+        newTask.classList.add('task');
+        newTask.dataset.key = this.id;
+
+        const taskTitle = document.createElement('div'); 
+        taskTitle.classList.add('task-title');
+        taskTitle.textContent = `${task.getTitle()}`;
+        newTask.appendChild(taskTitle);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-task-btn');
+        deleteButton.textContent = 'X';
+        newTask.appendChild(deleteButton);
+
+        document.querySelector('.tasks').appendChild(newTask);
     }
     
     deleteTask(task) {
@@ -19,6 +37,14 @@ class Project {
 
     getProjectName() {
         return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getTasks() {
+        return this.tasks;
     }
 }
 
