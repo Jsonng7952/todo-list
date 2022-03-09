068c4611a74a7b536a033397807ad70c93215768
Jason Ng <jason.ng7952@gmail.com>
2022-03-09 18:23:48 -0500

    Changed Task class to only use the title for now

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,43 +1,17 @@
 class Task {
 
-    constructor(title, description, dueDate, priority) {
+    constructor(title) {
         this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
     }
 
     setTitle(title) {
         this.title = title;
     }
 
-    setDescription(description) {
-        this.description = description;
-    }
-
-    setDate(dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    setPriority(priority) {
-        this.priority = priority;
-    }
-
     getTitle() {
         return this.title;
     }
 
-    getDescription() {
-        return this.description;
-    }
-
-    getDate() {
-        return this.dueDate;
-    }
-
-    getPriority() {
-        return this.priority;
-    }
 }
 
 export default Task;
